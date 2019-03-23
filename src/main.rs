111f0322ecfaefe8d10951cb912d427cacf7941d
JarateKing <scmacintosh@upei.ca>
2019-03-23 11:05:45 -0300

    added input loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+	
+	while input != "q"
+	{
+		input.clear();
+		io::stdin().read_line(&mut input).expect("Readline failed");
+		input = input.trim().to_string();
+		
+		println!("{}", input);
+	}
 }
