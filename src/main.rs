fd33ddb634ad348f8791f8f11d3b5fd077ed112c
JarateKing <scmacintosh@upei.ca>
2019-03-23 11:17:04 -0300

    simplified loop process

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
+	let mut is_looping = true;
 	
-	while input != "q" {
-		input = get_input();
+	while is_looping {
+		let mut input = get_input();
+		
+		if input == "q" || input == "quit" {
+			is_looping = false;
+		}
 		
 		println!("{}", input);
 	}
@@ -13,6 +17,8 @@ fn main() {
 fn get_input() -> std::string::String {
 	let mut input = String::new();
 	io::stdin().read_line(&mut input).expect("Readline failed");
-	input = input.trim().to_string();
+	input = input.trim().to_lowercase().to_string();
 	return input;
 }
+
+
