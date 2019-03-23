f1e6fe2816d64397c63eeaac4e89aa0e774fc9ba
JarateKing <scmacintosh@upei.ca>
2019-03-23 11:10:05 -0300

    moved input handling into own function

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ use std::io;
 fn main() {
     let mut input = String::new();
 	
-	while input != "q"
-	{
-		input.clear();
-		io::stdin().read_line(&mut input).expect("Readline failed");
-		input = input.trim().to_string();
+	while input != "q" {
+		input = get_input();
 		
 		println!("{}", input);
 	}
+}
+
+fn get_input() -> std::string::String {
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).expect("Readline failed");
+	input = input.trim().to_string();
+	return input;
 }
